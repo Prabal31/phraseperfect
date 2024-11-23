75a8b0339a8c3543d5bc5021fe0440fe724c18a5
Prabal Manchanda <123109839+Prabal31@users.noreply.github.com>
2024-11-23 12:29:15 -0500

    Add account icon to Header component for user profile access

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { MdOutlineAccountCircle } from "react-icons/md";
 
 const Header = () => {
-  return (
-    <header style={styles.header}>
-      <h1 style={styles.logo}>PhrasePerfect</h1>
-    </header>
-  );
-};
+    return (
+        <header style={styles.header}>
+          <h1 style={styles.logo}>PhrasePerfect</h1>
+          <span style={styles.account_logo}><MdOutlineAccountCircle style={styles.icon} /></span>
+        </header>
+      );
+    };
 
 const styles = {
   header: {
@@ -23,6 +25,12 @@ const styles = {
     fontSize: "1.9rem",
     fontWeight: "bold",
     margin: 0,
+  },
+  account_logo: {
+    marginLeft: "auto",
+    fontSize: "1.9rem",
+    color:"black"
+
   },
 };
 
