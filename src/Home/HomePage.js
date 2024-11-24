96df5ec0789de6646557749425050ee000453b5d
Prabal Manchanda <123109839+Prabal31@users.noreply.github.com>
2024-11-24 17:24:01 -0500

    Remove unnecessary blank lines in HomePage component for cleaner code

--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -1,5 +1,3 @@
-
-
 import React from "react";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import { FaFileAlt, FaCheckCircle, FaRobot, FaQuoteRight, FaLanguage, FaPenNib } from "react-icons/fa";
