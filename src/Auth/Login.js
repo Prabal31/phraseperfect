4ea1604c970ed7360b760d61310f4902df13e875
Prabal Manchanda <123109839+Prabal31@users.noreply.github.com>
2024-11-25 17:12:58 -0500

    Pass dark mode state to LoginPage component for consistent theming

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const LoginPage = () => {
+const LoginPage = ({isDarkMode}) => {
   return (
     <div style={styles.loginContainer}>
       <div style={styles.loginBox}>
